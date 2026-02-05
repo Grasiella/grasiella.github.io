91de53cec9f5b5fecd5ea7fe9eae50e1c8b685d4
Grasiela Ferreira <grasiela@jetbov.com>
2026-02-05 14:57:38 -0300

    deploy setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 p-6">
+      <h1 className="text-4xl font-bold">
+        contador nuclear da amizade ☢️💖
+      </h1>
+
+      <p>
+        faltam X dias para o reencontro internacional 🇧🇷✈️🇩🇪
+      </p>
+    </div>
+  )
+}
+
